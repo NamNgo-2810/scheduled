31e2d64557d93805a254be4bc8b7a72e78174ebc
NamNgo-2810 <henrynam28@gmail.com>
2023-05-29 21:14:00 +0700

    feat: initialize scheduling process

---
+++ jobs.js
@@ -0,0 +1,71 @@
+const connection = require("./database");
+const Rating = require("./models/rating");
+
+const updateRatings = async () => {
+    try {
+        const oneMinuteAgo = new Date(Date.now() - 60000);
+
+        // Format the timestamp for the MySQL query
+        const formattedTimestamp = oneMinuteAgo
+            .toISOString()
+            .slice(0, 19)
+            .replace("T", " ");
+
+        // Query to retrieve newly added comments and their ratings
+        const query = `select hotel_id, rate, count(*) from comments where created_at > '${formattedTimestamp}' group by hotel_id, rate`;
+
+        connection.query(query, async (error, results) => {
+            if (error) throw error;
+            if (results.length == 0) {
+                console.log("No new rating");
+            } else {
+                console.log("New ratings: ", results.length);
+            }
+
+            for (const result of results) {
+                const { hotel_id, rate, count } = result;
+
+                // Find the existing ratings for the hotel in MongoDB
+                let existingRatings = await Rating.findOne({
+                    hotelId: hotel_id,
+                });
+
+                // If ratings exist, update the corresponding rating count
+                if (existingRatings) {
+                    if (rate >= 4.5) existingRatings.wonderful += count;
+                    else if (rate >= 3.5) existingRatings.good += count;
+                    else if (rate >= 2.5) existingRatings.average += count;
+                    else if (rate >= 1.5) existingRatings.poor += count;
+                    else existingRatings.terrible += count;
+
+                    console.log(existingRatings);
+                    // await existingRatings.save();
+                }
+                // If ratings don't exist, create a new document with the ratings
+                else {
+                    let newRatings = {
+                        hotelId: hotel_id,
+                        wonderful: 0,
+                        good: 0,
+                        average: 0,
+                        poor: 0,
+                        terrible: 0,
+                    };
+
+                    if (rate >= 4.5) newRatings.wonderful += count;
+                    else if (rate >= 3.5) newRatings.good += count;
+                    else if (rate >= 2.5) newRatings.average += count;
+                    else if (rate >= 1.5) newRatings.poor += count;
+                    else newRatings.terrible += count;
+
+                    console.log(newRatings);
+                    // await newRatings.save();
+                }
+            }
+        });
+    } catch (error) {
+        console.log("Error:", error);
+    }
+};
+
+setInterval(updateRatings, process.env.RATING_INTERVAL);
